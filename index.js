b2f6375b871a8866d20f0b175802e5a11aec1d2e
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-14 14:15:34 -0400

    Create funcitons to move dodger

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+
+function moveDodgerLeft() {
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight () {
+  if (left < 360)
+    dodger.style.left = `${left + 1}px`;
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "Arrow Right") {
+    moveDodgerRight
+  }
+});
